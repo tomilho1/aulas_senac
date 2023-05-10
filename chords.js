05c44263f8b79d80f31b53666c236e52390109ee
Tom Francisco da Rosa <123115729+tomilho1@users.noreply.github.com>
2023-05-10 11:32:26 -0300

    :)

--- chords.js
+++ chords.js
@@ -49,17 +49,44 @@ function librarySearch(array_x, bigarray) {
     return index
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////
+// Elimina oitavas.
+function twelvefy(array_x) {
+    for (step = 0; step < array_x.length; step++) {
+        array_x[step] = array_x[step] % 12
+    }
+    return array_x
+}
 
-let my_chord = [0,4,7,11]
-let intervals = getIntervals(my_chord) // [4,3,4]
+// Formata o acorde, eliminando oitavas e ordenando as notas.
+function formatChord(array_x) {
+    let array_b = []
+    array_x = twelvefy(array_x)
+    for (step = 0; step < 12; step++) {
+        if ((array_x.includes(step)) === true) {
+            array_b.push(step)
+        }
+        else { }
+    }
+    return array_b
+}
 
-let chordnames = ["major", "minor", "diminished", "augmented","major seventh"]
-let chordlibrary = [[4, 3], [3, 4], [3, 3], [4, 4], [4,3,4]]
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Biblioteca
 
-console.log(librarySearch(intervals, chordlibrary))
+let chordnames = ["major", "minor", "diminished", "augmented", "major seventh"]
+let chordlibrary = [[4, 3], [3, 4], [3, 3], [4, 4], [4, 3, 4]]
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Entrada //
+
+let my_chord = [0,7,4,0,12,24]
+let sys_chord = formatChord(my_chord)
+intervals = getIntervals(sys_chord)
+
+
+
+console.log("Acorde de entrada", my_chord)
+console.log("Acorde formatado", formatChord(my_chord))
+console.log("Intervalos", intervals)
+console.log("Index do acorde na biblioteca:", librarySearch(intervals, chordlibrary))
 console.log(chordnames[librarySearch(intervals, chordlibrary)])
-
-
-
-
